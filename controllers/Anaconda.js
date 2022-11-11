9f68b545a46a8150f390a66a68c3413e9edc2971
kpk9999 <112146534+kpk9999@users.noreply.github.com>
2022-11-11 12:38:13 -0600

    lab

--- controllers/Anaconda.js
+++ controllers/Anaconda.js
@@ -58,3 +58,32 @@ exports.Anaconda_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   exports.Anaconda_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Anaconda.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+exports.Anaconda_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Anaconda.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Ananconda_color)  
+               toUpdate.Ananconda_color = req.body.Ananconda_color; 
+        if(req.body.Ananconda_petname) toUpdate.Ananconda_petname = req.body.Ananconda_petname; 
+        if(req.body.Ananconda_price) toUpdate.Ananconda_price = req.body.Ananconda_price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
