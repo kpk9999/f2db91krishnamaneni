0ecb452f5bb413fc1a3696ed329989fd117ba344
kpk9999 <112146534+kpk9999@users.noreply.github.com>
2022-11-16 22:32:40 -0600

    commmit

--- controllers/Anaconda.js
+++ controllers/Anaconda.js
@@ -1,45 +1,45 @@
 var Anaconda = require('../models/Anaconda');
 // List of all Anacondas
-exports.Anaconda_list = function(req, res) {
- res.send('NOT IMPLEMENTED: Anaconda list');
+exports.Anaconda_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: Anaconda list');
 };
 // for a specific Anaconda.
-exports.Anaconda_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Anaconda detail: ' + req.params.id);
+exports.Anaconda_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Anaconda detail: ' + req.params.id);
 };
 // Handle Anaconda create on POST.
-exports.Anaconda_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Anaconda create POST');
+exports.Anaconda_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Anaconda create POST');
 };
 // Handle Anaconda delete form on DELETE.
-exports.Anaconda_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Anaconda delete DELETE ' + req.params.id);
+exports.Anaconda_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Anaconda delete DELETE ' + req.params.id);
 };
 // Handle Anaconda update form on PUT.
-exports.Anaconda_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Anaconda update PUT' + req.params.id);
+exports.Anaconda_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Anaconda update PUT' + req.params.id);
 };
-exports.Anaconda_list = async function(req, res) {
-    try{
-    theAnacondas = await Anaconda.find();
-    res.send(theAnacondas);
+exports.Anaconda_list = async function (req, res) {
+    try {
+        theAnacondas = await Anaconda.find();
+        res.send(theAnacondas);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
-   exports.Anaconda_view_all_Page = async function(req, res) {
-    try{
-    theAnacondas = await Anaconda.find();
-    res.render('Anaconda', { title: 'Anaconda Search Results', results: theAnacondas });
+};
+exports.Anaconda_view_all_Page = async function (req, res) {
+    try {
+        theAnacondas = await Anaconda.find();
+        res.render('Anaconda', { title: 'Anaconda Search Results', results: theAnacondas });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
-   exports.Anaconda_create_post = async function(req, res) {
+};
+exports.Anaconda_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Anaconda();
     // We are looking for a body, since POST does not have query parameters.
@@ -49,41 +49,101 @@ exports.Anaconda_list = async function(req, res) {
     document.Ananconda_color = req.body.Ananconda_color;
     document.Ananconda_petname = req.body.Ananconda_petname;
     document.Ananconda_price = req.body.Ananconda_price;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
-   exports.Anaconda_detail = async function(req, res) { 
-    console.log("detail"  + req.params.id) 
-    try { 
-        result = await Anaconda.findById( req.params.id) 
-        res.send(result) 
-    } catch (error) { 
-        res.status(500) 
-        res.send(`{"error": document for id ${req.params.id} not found`); 
-    } 
-}; 
-exports.Anaconda_update_put = async function(req, res) { 
+};
+exports.Anaconda_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Anaconda.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+exports.Anaconda_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`) 
-    try { 
-        let toUpdate = await Anaconda.findById( req.params.id) 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Anaconda.findById(req.params.id)
         // Do updates of properties 
-        if(req.body.Ananconda_color)  
-               toUpdate.Ananconda_color = req.body.Ananconda_color; 
-        if(req.body.Ananconda_petname) toUpdate.Ananconda_petname = req.body.Ananconda_petname; 
-        if(req.body.Ananconda_price) toUpdate.Ananconda_price = req.body.Ananconda_price; 
-        let result = await toUpdate.save(); 
-        console.log("Sucess " + result) 
-        res.send(result) 
-    } catch (err) { 
-        res.status(500) 
+        if (req.body.Ananconda_color)
+            toUpdate.Ananconda_color = req.body.Ananconda_color;
+        if (req.body.Ananconda_petname) toUpdate.Ananconda_petname = req.body.Ananconda_petname;
+        if (req.body.Ananconda_price) toUpdate.Ananconda_price = req.body.Ananconda_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`); 
-    } 
-}; 
+failed`);
+    }
+};
+// Handle Anaconda delete on DELETE.
+exports.Anaconda_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Anaconda.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+// Handle a show one view with id specified by query
+exports.Anaconda_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Anaconda.findById(req.query.id)
+        res.render('Anacondadetail',
+            { title: 'Anaconda Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.Anaconda_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('Anacondacreate', { title: 'Anaconda Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.Anaconda_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Anaconda.findById(req.query.id)
+        res.render('Anacondaupdate', { title: 'Anaconda Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.Anaconda_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Anaconda.findById(req.query.id)
+        res.render('Anacondadelete', {
+            title: 'Anaconda Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/Anaconda.js
+++ routes/Anaconda.js
@@ -14,5 +14,10 @@ const Anaconda_controlers= require('../controllers/Anaconda');
 var router = express.Router();
 /* GET Anacondas */
 router.get('/', Anaconda_controlers.Anaconda_view_all_Page );
+/* GET detail Anaconda page */
+router.get('/detail', Anaconda_controlers.Anaconda_view_one_Page);
+router.get('/create', Anaconda_controlers.Anaconda_create_Page);
+router.get('/update', Anaconda_controlers.Anaconda_update_Page);
+router.get('/delete', Anaconda_controlers.Anaconda_delete_Page);
 module.exports = router;
 module.exports = router;
